e28af63eddcab8f9b6d1a435b30f37d482ae56a9
André Marcus <167018323+AndremRb@users.noreply.github.com>
2025-06-02 16:31:06 -0300

    Update medico.service.ts

--- src/front/SimplificaTerapia_1/SimplificaTerapia/src/app/services/medico.service.ts
+++ src/front/SimplificaTerapia_1/SimplificaTerapia/src/app/services/medico.service.ts
@@ -1,18 +1,24 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Medico } from '../models/medico';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 @Injectable({ providedIn: 'root' })
 export class MedicoService {
   private apiUrl = 'https://localhost:7222/api/medicos';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  
-  getMedicos(): Observable<Medico[]> {
-    return this.http.get<Medico[]>(this.apiUrl);
+
+  getMedicos(nome?: string): Observable<Medico[]> {
+    let params = new HttpParams();
+    if (nome) {
+      params = params.set('nome', nome);
+    }
+
+    return this.http.get<Medico[]>(this.apiUrl, { params });
   }
+
 
   getMedico(id: number): Observable<Medico> {
     return this.http.get<Medico>(`${this.apiUrl}/${id}`);
