7fcf678423348d041e9cf9c63792f7b7616872fa
André Marcus <167018323+AndremRb@users.noreply.github.com>
2025-06-16 16:29:18 -0300

    Atualizando o login

--- src/front/SimplificaTerapia_1/SimplificaTerapia/src/app/login/login.component.ts
+++ src/front/SimplificaTerapia_1/SimplificaTerapia/src/app/login/login.component.ts
@@ -5,6 +5,7 @@ import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angula
 import { RouterModule } from '@angular/router';
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { LoginStateService } from '../login-state.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -16,36 +17,25 @@ import { LoginStateService } from '../login-state.service';
     MatTooltipModule,
   ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-
   loginForm!: FormGroup;
-  tipoSelecionado: string = 'paciente';  
-  mostrarAlerta: boolean = false;
+  tipoSelecionado: string = 'paciente';
+  mostrarErro: boolean = false;
 
   constructor(
     private fb: FormBuilder,
     private loginState: LoginStateService,
-    private router: Router
-  ) {}
+    private router: Router,
+    private http: HttpClient
+  ) { }
 
   ngOnInit(): void {
-    this.mostrarAlerta = true;  
-    setTimeout(() => {
-      this.mostrarAlerta = false;
-    }, 5000);
-    
     this.loginForm = this.fb.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
+      Email: ['', [Validators.required, Validators.email]],
+      Senha: ['', Validators.required]
     });
-
-  }
-
-  onSubmit(): void {
-    this.loginState.logado = true;
-    this.router.navigate(['/']);
   }
 
   isChecked(tipo: string): boolean {
@@ -54,5 +44,28 @@ export class LoginComponent {
 
   selecionarTipo(tipo: string): void {
     this.tipoSelecionado = tipo;
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.invalid) return;
+
+    const { Email, Senha } = this.loginForm.value;
+
+    this.http.post<any>('https://localhost:7222/api/auth/login', {
+      email: Email,
+      senha: Senha,
+      tipo: this.tipoSelecionado
+    }).subscribe({
+      next: (res) => {
+        localStorage.setItem('usuarioLogado', JSON.stringify(res));
+        this.loginState.logado = true;
+
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        this.mostrarErro = true;
+        setTimeout(() => this.mostrarErro = false, 5000);
+      }
+    });
   }
 }
