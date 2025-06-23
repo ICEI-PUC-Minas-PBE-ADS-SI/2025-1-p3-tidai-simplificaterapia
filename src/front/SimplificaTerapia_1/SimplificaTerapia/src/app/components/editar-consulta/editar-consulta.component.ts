6a1ea526d32741fc1621538db204ed1c5b3b1e15
André Marcus <167018323+AndremRb@users.noreply.github.com>
2025-06-23 11:36:07 -0300

    Agora ta editando a consulta certo

--- src/front/SimplificaTerapia_1/SimplificaTerapia/src/app/components/editar-consulta/editar-consulta.component.ts
+++ src/front/SimplificaTerapia_1/SimplificaTerapia/src/app/components/editar-consulta/editar-consulta.component.ts
@@ -2,7 +2,6 @@ import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-// Imports do Angular Material para o formulário
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -25,23 +24,25 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
   styleUrls: ['./editar-consulta.component.scss']
 })
 export class EditarConsultaComponent implements OnInit {
-  // 1. Recebe a consulta selecionada do componente pai
   @Input() consultaParaEditar: any;
 
-  // 2. Cria eventos para se comunicar com o componente pai
   @Output() fechar = new EventEmitter<void>();
   @Output() salvar = new EventEmitter<any>();
 
-  editForm!: FormGroup; // Nosso formulário de edição
+  editForm!: FormGroup;
 
-  // Arrays para os selects do formulário
+  duracoes = [
+    { value: '30', viewValue: '30 minutos' },
+    { value: '60', viewValue: '1 hora' },
+    { value: '90', viewValue: '1 hora e 30 minutos' },
+    { value: '120', viewValue: '2 horas' }
+  ];
   situacoes = ['Agendada', 'Finalizada', 'Cancelada'];
   tiposAtendimento = ['Presencial', 'Online'];
 
   constructor(private fb: FormBuilder) {}
 
   ngOnInit(): void {
-    // 3. Cria a estrutura do formulário
     this.editForm = this.fb.group({
       data_consulta: ['', Validators.required],
       horario: ['', Validators.required],
@@ -51,13 +52,12 @@ export class EditarConsultaComponent implements OnInit {
       observacoes: ['']
     });
 
-    // 4. Preenche o formulário com os dados da consulta recebida
     if (this.consultaParaEditar && this.consultaParaEditar.dadosOriginais) {
       const dados = this.consultaParaEditar.dadosOriginais;
       this.editForm.patchValue({
-        data_consulta: new Date(dados.data_consulta), // Converte a string para um objeto Date
+        data_consulta: new Date(dados.data_consulta),
         horario: dados.horario,
-        duracao: dados.duracao.toString(),
+        duracao: dados.duracao ? dados.duracao.toString(): '',
         situacao: dados.situacao,
         tipo_atendimento: dados.tipo_atendimento,
         observacoes: dados.observacoes
@@ -65,26 +65,21 @@ export class EditarConsultaComponent implements OnInit {
     }
   }
 
-  // 5. Função chamada quando o formulário é enviado
   salvarEdicao(): void {
     if (this.editForm.invalid) {
       alert('Por favor, preencha todos os campos obrigatórios.');
       return;
     }
 
-    // Pega os dados originais e atualiza com os valores do formulário
     const dadosAtualizados = {
       ...this.consultaParaEditar.dadosOriginais,
       ...this.editForm.value
     };
 
-    // Emite o evento 'salvar' com os dados atualizados para o componente pai
     this.salvar.emit(dadosAtualizados);
   }
 
-  // 6. Função chamada para fechar o modal
   fecharModal(): void {
-    // Apenas emite o evento 'fechar'
     this.fechar.emit();
   }
 }
